66da89c576545e9b9bb165f4bca93b5f34982941
Rahad Rahman <rahman.rahad@yahoo.com>
2018-03-28 12:52:42 +0600

    Update registerServiceWorker.js

--- docs/registerServiceWorker.js
+++ docs/registerServiceWorker.js
@@ -1,15 +1,4 @@
 if ('serviceWorker' in navigator) {
-        window.addEventListener('load', function() {
-          navigator.serviceWorker.register('service-worker.js').then(function(registration) {
-            // Registration was successful
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-          }, function(err) {
-            // registration failed :(
-            console.log('ServiceWorker registration failed: ', err);
-          }).catch(function(err) {
-            console.log(err)
-          });
-        });
-      } else {
-        console.log('service worker is not supported');
-      }
+  navigator.serviceWorker.register('service-worker.js');
+   console.log("service worker registered")
+}
