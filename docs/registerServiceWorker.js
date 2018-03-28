4f5131a172adaa9e2831f86bf8f346b1ccd5dace
Rahad Rahman <rahman.rahad@yahoo.com>
2018-03-28 12:54:33 +0600

    Update registerServiceWorker.js

--- docs/registerServiceWorker.js
+++ docs/registerServiceWorker.js
@@ -1,4 +1,15 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker.js');
-   console.log("service worker registered")
-}
+  if ('serviceWorker' in navigator) {
+        window.addEventListener('load', function() {
+          navigator.serviceWorker.register('service-worker.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+          }, function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+          }).catch(function(err) {
+            console.log(err)
+          });
+        });
+      } else {
+        console.log('service worker is not supported');
+      }
