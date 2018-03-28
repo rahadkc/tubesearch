2bb8d221344e949e5ccc2c6f01bc1c352da3d668
Rahad Rahman <rahman.rahad@yahoo.com>
2018-03-28 12:48:56 +0600

    Update service-worker.js

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v1';
+const PRECACHE = 'my-pwa-cache-v1';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
@@ -23,7 +23,7 @@ const PRECACHE_URLS = [
   './', // Alias for index.html
   'style.css',
   'bundle.js',
-  'demo.js',
+  'registerServiceWorker.js',
   'logo.png'
 ];
 
